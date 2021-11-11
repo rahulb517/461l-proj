41a2c4ac8e1f8a535b67772808e7b57a8f2205cd
Haining Liu <haining52225@gmail.com>
2021-11-10 23:55:49 -0600

    homepage and redirects

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -10,7 +10,7 @@ import Dashboard from './components/Dashboard';
 import Logout from './components/Logout'
 import PrivateRoute from './components/PrivateRoute'
 import { AuthProvider } from './AuthContext';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import React from 'react';
 import './App.css';
 
@@ -31,27 +31,34 @@ function App() {
 					<Route path="/login">
 						<Login />
 					</Route>
-					<React.Fragment>
-						<Sidebar />
+					{/* <React.Fragment> */}
+						{/* <Sidebar /> */}
 						<PrivateRoute path="/dashboard">
+							<Sidebar />
 							<Dashboard />
 						</PrivateRoute>
 						<PrivateRoute path="/home">
+							<Sidebar />
 							<Dashboard />
 						</PrivateRoute>
 						<PrivateRoute path="/datasets">
+							<Sidebar />
 							<Datasets />
 						</PrivateRoute>
 						<PrivateRoute path="/projects">
+							<Sidebar />
 							<Projects />
 						</PrivateRoute>
 						<PrivateRoute path="/resources">
+							<Sidebar />
 							<Resources />
 						</PrivateRoute>
 						<PrivateRoute path="/logout">
+							<Sidebar />
 							<Logout />
 						</PrivateRoute>
-					</React.Fragment>
+					{/* </React.Fragment> */}
+					<Redirect to="/"/>
 					</Switch>
 				</BrowserRouter>
 			</div>
